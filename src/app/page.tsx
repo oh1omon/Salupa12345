94b67c0fceeef08d39fff084fea8213dca109f55
Egor1o <egorklo147@gmail.com>
2023-11-11 17:21:50 +0200

    added shuttle spinning feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,32 @@
 'use client'
 
-import SceneComponent from "./_components/SceneComponent"
-import { onRender, onSceneReady } from "./_components/scene"
+import SceneComponent from './_components/SceneComponent'
+import { onRender, onSceneReady } from './_components/scenespace'
 
 const Home = () => {
     return (
-        <div className="flex h-full w-100 flex-1 flex-col justify-center gap-1 md:w-full">
-            <header className='text-center'>
-                <h1 className="text-4xl">Robot<span className='text-emerald-500'>Guesser</span></h1>
+        <div className="w-100 flex h-full flex-1 flex-col justify-center gap-1 md:w-full">
+            <header className="text-center">
+                <h1 className="text-4xl">
+                    Robot<span className="text-emerald-500">Guesser</span>
+                </h1>
                 <h3>Guess who you are chatting with and win prizes!</h3>
             </header>
             <SceneComponent
                 antialias
                 onSceneReady={onSceneReady}
                 onRender={onRender}
-                engineOptions={undefined} adaptToDeviceRatio={undefined} sceneOptions={undefined}            />
-            <div className='text-center flex justify-center absolute m-auto left-0 right-0 bottom-8'>
-                <button className='border mr-4 p-2 rounded-lg border-emerald-500'>Become the guesser</button>
-                <button className='border p-2 rounded-lg border-emerald-500'>Play as the robot</button>
+                engineOptions={undefined}
+                adaptToDeviceRatio={undefined}
+                sceneOptions={undefined}
+            />
+            <div className="absolute bottom-8 left-0 right-0 m-auto flex justify-center text-center">
+                <button className="mr-4 rounded-lg border border-emerald-500 p-2">
+                    Become the guesser
+                </button>
+                <button className="rounded-lg border border-emerald-500 p-2">
+                    Play as the robot
+                </button>
             </div>
         </div>
     )
