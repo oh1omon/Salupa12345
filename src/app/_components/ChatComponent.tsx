b1d268ce4bce809006cdc4f8ad92f9ed223930df
Jan Pytel <jas.pytel@gmail.com>
2023-11-11 16:37:43 +0200

    Added send message icon to the ChatComponent

--- src/app/_components/ChatComponent.tsx
+++ src/app/_components/ChatComponent.tsx
@@ -81,8 +81,10 @@ const ChatComponent: React.FC = () => {
           />
           <div
             onClick={handleSubmit}
-            className="h-6 w-9 cursor-pointer self-center rounded bg-medium-green transition-transform duration-300 hover:brightness-90"
-          ></div>
+            className="h-7 w-7 p-1 cursor-pointer self-center rounded bg-medium-green transition-transform duration-300 hover:brightness-90"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none" className="w-full h-full"><path d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z" fill="currentColor"></path></svg>
+          </div>
         </div>
       </div>
     </div>
