d6ed19a0ce0b9ac4c4d0759fa3d18c7b1b413aaf
Kuchteq <mariusz.kuchta@triona.fi>
2023-11-11 17:38:19 +0200

    scenespaceupdate

--- src/app/_components/scenespace.ts
+++ src/app/_components/scenespace.ts
@@ -1,5 +1,7 @@
 import {
+    AbstractMesh,
     Color4,
+    CubeTexture,
     FlyCamera,
     HemisphericLight,
     Mesh,
@@ -11,7 +13,7 @@ import '@babylonjs/loaders/glTF'
 // import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
 
 let box: Mesh | undefined
-let rocket: Mesh | undefined
+let rocket: AbstractMesh 
 
 let x_pos: Number
 let y_pos: Number
@@ -24,19 +26,14 @@ let z_start: Number
 const onSceneReady = (scene: Scene) => {
     // This creates and positions a free camera (non-mesh)
     const camera = new FlyCamera('camera1', new Vector3(0, 1, -3), scene)
-
+    var envTexture = new CubeTexture("space", scene);
+    scene.createDefaultSkybox(envTexture, true, 1000);
     // This targets the camera to scene origin
     camera.setTarget(Vector3.Up())
 
-    const canvas = scene.getEngine().getRenderingCanvas()
 
     // This attaches the camera to the canvas
-    camera.attachControl(canvas, true)
-
-    SceneLoader.ImportMesh("", "animations/", "ship.glb", scene, function(newMeshes) {
-        // Set the target of the camera to the first imported mesh
-        const hero = newMeshes[0];
-    });
+    camera.attachControl(true)
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
@@ -45,15 +42,14 @@ const onSceneReady = (scene: Scene) => {
     scene.clearColor = new Color4(0, 0, 0, 0)
 
     // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 1.1
+    light.intensity = 0.1
 
-    SceneLoader.Append(
-        'animations/',
-        'rocket.glb',
-        scene,
-        function (loadedScene) {
-            // Get the root mesh of the loaded scene
-            rocket = scene.meshes[0]
+
+    SceneLoader.ImportMesh("", "animations/", "rocket.glb", scene, function(newMeshes) {
+        // Set the target of the camera to the first imported mesh
+            
+        if(newMeshes[0])
+            rocket = newMeshes[0]
             x_start = rocket.position.x
             y_start = rocket.position.y
             z_start = rocket.position.z
@@ -61,34 +57,7 @@ const onSceneReady = (scene: Scene) => {
             rocket.position.y = rocket.position.y + 1
             // Set the scaling of the root mesh to make the object smaller
             rocket.scaling.scaleInPlace(0.007)
-        }
-    )
-
-    // Our built-in 'box' shape.
-    // box = MeshBuilder.CreateBox('box', { size: 0.5 }, scene)
-
-    // Move the box upward 1/2 its height
-    // box.position.y = 1
-    // SceneLoader.ImportMesh("", "/animations/", "robot.glb", scene); //Empty string loads all meshes
-
-    // if (hero) {
-    //     hero.position.x = 1 + index;
-    //     hero.rotation = new Vector3(0, Math.PI / 16, 0);
-    // }
-
-    // SceneLoader.ImportMesh("", "animations/", "hooman.glb", scene, function(newMeshes) {
-    //     // Set the target of the camera to the first imported mesh
-    //     const hero = newMeshes[0];
-    //     if (hero) {
-    //         hero.position.x = -1-index;
-    //         hero.rotation = new Vector3(0, Math.PI / 16, 0);
-    //         hero.scaling.scaleInPlace(0.5);
-    //     }
-    //     const idleAnim = scene.getAnimationGroupByName("defeated");
-    //     // idleAnim?.stop();
-
-    // Our built-in 'ground' shape.
-    // MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene)
+    });
 }
 
 /**
