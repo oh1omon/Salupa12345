087d4b327c09e3a94e84fae1da99edaea1ea6191
Egor1o <egorklo147@gmail.com>
2023-11-11 11:41:47 +0200

    Scene and basic logic has been added in scene.ts and SceneComponent.jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
+import { onRender, onSceneReady } from './scene.ts'
+import SceneComponent from './SceneComponent'
 
 const Home = () => {
-
     return (
         <div className="flex h-full w-96 max-w-4xl flex-1 flex-col justify-center gap-1 md:w-full">
             <p>Hello</p>
+            <SceneComponent
+                antialias
+                onSceneReady={onSceneReady}
+                onRender={onRender}
+                id="my-canvas"
+            />
         </div>
     )
 }
